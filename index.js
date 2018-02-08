9aeea60bdb56fbb98567d39d66a15274f23d309f
Nurs <nursultan.2027@gmail.com>
2018-02-08 12:06:59 +0600

    First commit

---
+++ index.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+var bodyParser = require('body-parser');
+ 
+ 
+app.use(bodyParser.urlencoded({
+    extended : true,
+    limit : '50mb'
+}));
+ 
+ 
+app.use(bodyParser.json({
+    limit : '50mb'
+}));
+
+app.use(bodyParser.json());
+
+app.post('/asd', function (req, res){
+    console.log(req.body.name);
+});
+
+app.use(express.static(path.join(__dirname,'client')));
+app.get('/', function (req, res) {
+  res.sendFile(__dirname+ '/client/index.html');
+}); 
+ 
+app.use(function (err,req,res,next) {
+    console.log(err);
+    res.send('This is error');
+});
+ 
+app.listen(2030, function () {
+  console.log('Example app listening on port 2028!')
+});
