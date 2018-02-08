2acaabee4f0381fd912ee83dc57c7a97ca49812e
Nurs <nursultan.2027@gmail.com>
2018-02-08 14:47:38 +0600

    last commit

--- index.js
+++ index.js
@@ -12,37 +12,44 @@ app.use(bodyParser.urlencoded({
     extended : true,
     limit : '50mb'
 }));
- 
- 
+  
 app.use(bodyParser.json({
     limit : '50mb'
 }));
 
 app.use(bodyParser.json());
 
-app.post('/asd', function (req, res){
-	console.log(req.body.name);
-
-
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("mydb");
-  var myobj = { name: req.body.name, 
-  				surname: req.body.surname 
-  			};
-  dbo.collection("customers").insertOne(myobj, function(err, res) {
-    if (err) throw err;
-    console.log("1 document inserted");
-    db.close();
-  });
-});
-
-});
-
 app.use(express.static(path.join(__dirname,'client')));
 app.get('/', function (req, res) {
   res.sendFile(__dirname+ '/client/index.html');
 });  
+
+app.post('/asd', function (req, res){
+	MongoClient.connect(url, function(err, db) {
+		  if (err) throw err;
+		  var dbo = db.db("mydb");
+		  var myobj = 
+		  { 
+		  	name: req.body.name, 
+		  	surname: req.body.surname,
+		    thirdname: req.body.thirdname,
+		    sex: req.body.sex,
+		    photo: req.body.photo,
+		    dol: req.body.dol,
+		    podr: req.body.podr,
+		    dos1: req.body.dos1,
+		    dos2: req.body.dos2,
+		    dos3: req.body.dos3,
+		    dos4: req.body.dos4
+		  };
+		  dbo.collection("customers").insertOne(myobj, function(err, res) {
+		    if (err) throw err;
+		    console.log(myobj.name+" "+ myobj.podr +" "+" - inserted");
+		    db.close();
+		  });
+		});
+});
+
 app.use(function (err,req,res,next) {
     console.log(err);
     res.send('This is error');
